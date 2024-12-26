a23d62e071839def28e0e8f5391e6fcc20ac2b0d
Sky28022004 <ramasyerif93@gmail.com>
2024-12-27 02:09:50 +0700

    pwd

---
+++ about.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HotelKu.Co - ABOUT</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+    <?php require('inc/links.php') ?>
+</head>
+<body class="bg-light">
+
+    <?php require('inc/header.php'); ?>
+
+    <div class="my-5 px-4">
+        <h2 class="fw-bold h-font text-center">ABOUT US</h2>
+        <div class="h-line bg-dark"></div>
+        <p class="text-center mt-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Est veritatis, reprehenderit tempora <br> amet molestias aliquid
+            accusamus aut tenetur ratione eveniet.
+        </p>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-5 mb-4s">
+                <h3 class="mb-3">Lorem ipsum dolor sit.</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Fugit excepturi labore libero quos. Ullam, quisquam voluptatum!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Fugit excepturi labore libero quos. Ullam, quisquam voluptatum!
+                </p>
+            </div>
+            <div class="col-lg-5 col-md-5">
+                <img src="images/" alt="">
+            </div>
+        </div>
+    </div>
+
+    <?php require('inc/footer.php') ?>
+</body>
+</html>
